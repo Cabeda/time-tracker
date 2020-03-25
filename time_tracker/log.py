c28f32210dbc3184e797db23864b5eb306560587
José Cabeda <jecabeda@gmail.com>
2020-03-25 18:28:53 +0000

    Move from pipenv to poetry. Add logs and pomodor options

--- time_tracker/log.py
+++ time_tracker/log.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 import os
 
+
 class Logger:
     def __init__(self):
         # Load envs
@@ -14,16 +15,10 @@ class Logger:
 
         FORMAT = "%(asctime)-10s | %(levelname)s | %(message)s"
 
-        logger = logging.getLogger()
-
         if os.getenv("ENV") == "dev":
-            log_file = (
-                "logs/"
-                + (datetime.today()).strftime("%Y-%m-%d")
-                + ".log"
-            )
-            logging.basicConfig(filename=log_file, format=FORMAT, level=logging.INFO)
-        else:
             logging.basicConfig(
                 format=FORMAT, level=logging.INFO, handlers=[logging.StreamHandler()]
             )
+        else:
+            log_file = "logs/" + (datetime.today()).strftime("%Y-%m-%d") + ".log"
+            logging.basicConfig(filename=log_file, format=FORMAT, level=logging.INFO)
