5a76eb6359553f037eacda2482d0c2ab10a0cb63
José Cabeda <jecabeda@gmail.com>
2020-03-30 14:40:38 +0100

    Add stdout option to log command

--- time_tracker/main.py
+++ time_tracker/main.py
@@ -92,14 +92,19 @@ def notify(title, text):
 
 @app.command()
 def log(
-    last_log: bool = typer.Option(False, "--last", "-l", help="Open last log file"),
+    last_log: bool = typer.Option(True, "--last", "-l", help="Open last log file"),
+    output: bool = typer.Option(
+        False, "--output", "-o", help="Return log file content to terminal"
+    ),
 ):
     if last_log:
-        list_of_files = glob.glob(
-            "./logs/*.log"
-        )
+        list_of_files = glob.glob("./logs/*.log")
         latest_file = max(list_of_files, key=os.path.getctime)
-        os.system(f"open {latest_file}")
+        if output:
+            with open(latest_file, "r") as file:
+                print(file.read())
+        else:
+            os.system(f"open {latest_file}")
     else:
         os.system("open logs")
 
